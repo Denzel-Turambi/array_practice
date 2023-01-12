65f19fc521400db66318cf645ff8b29f2044017e
Denzel Turambi <dturambi26@gmail.com>
2023-01-11 20:32:11 -0700

    Add index ex

--- arrays.js
+++ arrays.js
@@ -1,6 +1,9 @@
 var names = ["Denz", "Chris", "Tim", "Rich"];
 var ages = [22, 18, 19, 27];
 var booleans = [true, true, false, true];
+// "Denz" is the 0 element in the names array
+// 27 is the 3 element in the ages array
+// false is the 2 element in the booleans array
 
 console.log(names.pop());
 // pop() removes the last element from the array and prints the element as an output.
@@ -11,3 +14,4 @@ console.log(ages);
 console.log(booleans.unshift(false, true));
 console.log(booleans);
 // unshift() adds one or more elements to the beginning of an array.
+
